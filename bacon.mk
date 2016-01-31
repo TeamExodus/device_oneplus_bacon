61924aab5aca09ca725d0b369af03368762d112d
Alex Naidis <alex.naidis@linux.com>
2016-01-31 17:59:18 +0100

    bacon: remove old nfc extras configs
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- bacon.mk
+++ bacon.mk
@@ -144,16 +144,7 @@ PRODUCT_PACKAGES += \
     libxml2
 
 # NFC
-ifeq ($(TARGET_BUILD_VARIANT),user)
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access.xml
-else
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access_debug.xml
-endif
-PRODUCT_COPY_FILES += \
-    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
-
 PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
     NfcNci \
     nfc_nci.pn54x.default \
     Tag
